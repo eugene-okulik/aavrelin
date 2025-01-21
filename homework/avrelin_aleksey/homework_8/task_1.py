10bc4e6e6b852322bbadacb0936e3690596eb59e
Avrelin Aleksey <aavrelin@wellink.ru>
2025-01-21 20:32:01 +0300

    Исправлена стилистическая ошибка в файлe task_1

--- homework/avrelin_aleksey/homework_8/task_1.py
+++ homework/avrelin_aleksey/homework_8/task_1.py
@@ -6,7 +6,7 @@ salary_input = int(input("Введи целое число: "))
 
 def two_case(salary):
     bonus = random.choice([True, False])
-    bonus_random = random.randint(1,100000)
+    bonus_random = random.randint(1, 100000)
     salary_bonus = salary + int(bonus_random)
     if bonus is True:
         print(f"{salary}, {bonus} - '${salary_bonus}'")
