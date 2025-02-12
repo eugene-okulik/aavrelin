01cb38388cdff9569397f2422c18e6941f1c5f7a
Avrelin Aleksey <aavrelin@wellink.ru>
2025-02-13 00:13:56 +0300

    Исправлены стилистические ошибки в файле hometask.py

--- homework/avrelin_aleksey/homework_16/hometask.py
+++ homework/avrelin_aleksey/homework_16/hometask.py
@@ -4,7 +4,7 @@ import mysql.connector as mysql
 import dotenv
 
 
-dotenv = dotenv.load_dotenv()
+dotenv.load_dotenv()
 
 db = mysql.connect(
     user=os.getenv('DB_USER'),
@@ -35,7 +35,6 @@ query = """
 """
 cursor.execute(query)
 student_info = cursor.fetchall()
-# print(student_info)
 
 file = 'data.csv'
 base_path = os.path.dirname(__file__)
@@ -43,7 +42,6 @@ homework_path = os.path.dirname(os.path.dirname(os.path.dirname(base_path)))
 new_file_path = os.path.join(
     homework_path, 'homework', 'eugene_okulik', 'Lesson_16', 'hw_data', file
 )
-
 
 with open(new_file_path, newline='') as csv_new:
     file = csv.reader(csv_new)
@@ -56,23 +54,25 @@ for csv_row in csv_data:
         name, second_name, group_title,
         book_title, subject_title, lesson_title,
         mark_value
-        ) = csv_row
+    ) = csv_row
     found_in_base = "Не найдено"
     for base_row in student_info:
         if (base_row['student_name'] == name
-            and base_row['student_second_name'] == second_name
-            and base_row['group_title'] == group_title
-            and base_row['book_title'] == book_title
-            and base_row['subject_title'] == subject_title
-            and base_row['lesson_title'] == lesson_title
-            and str(base_row['mark_value']) == mark_value):
-                found_in_base = "Найдено"
-                break
+                and base_row['student_second_name'] == second_name
+                and base_row['group_title'] == group_title
+                and base_row['book_title'] == book_title
+                and base_row['subject_title'] == subject_title
+                and base_row['lesson_title'] == lesson_title
+                and str(base_row['mark_value']) == mark_value):
+            found_in_base = "Найдено"
+            break
 
     if found_in_base == "Не найдено":
-        print(f'name: {name}, second_name: {second_name}, group_title: {group_title},'
-              f' book_title: {book_title}, subject_title: {subject_title}, lesson_title: {lesson_title}, '
-                  f'mark_value: {mark_value}')
+        print(
+            f'name: {name}, second_name: {second_name}, group_title: {group_title}, '
+            f'book_title: {book_title}, subject_title: {subject_title}, lesson_title: {lesson_title}, '
+            f'mark_value: {mark_value}'
+        )
 
 cursor.close()
 db.close()
