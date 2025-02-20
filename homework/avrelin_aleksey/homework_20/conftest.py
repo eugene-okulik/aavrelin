c522be45af29795b1ff35748b98af7d2051bd564
Avrelin Aleksey <aavrelin@wellink.ru>
2025-02-20 23:53:38 +0300

    Исправлены стилистические ошибки в conftest.py и test_api_requests.py

--- homework/avrelin_aleksey/homework_20/conftest.py
+++ homework/avrelin_aleksey/homework_20/conftest.py
@@ -22,13 +22,13 @@ def create_and_delete_object():
     with allure.step("Создаем объект методом post"):
         body = {"name": "Новый объект", "data": {"объект": "ВОЗДУХ"}}
         headers = {"Content-Type": "application/json"}
-        response = requests.post('http://167.172.172.115:52353/object',
-                                 json=body,
-                                 headers=headers)
-        post_id_for_test = response.json()['id']
+        response = requests.post(
+            "http://167.172.172.115:52353/object", json=body, headers=headers
+        )
+        post_id_for_test = response.json()["id"]
         yield post_id_for_test
         with allure.step("Удаление тестовых данных"):
-            requests.delete(f'http://167.172.172.115:52353/object/{post_id_for_test}')
+            requests.delete(f"http://167.172.172.115:52353/object/{post_id_for_test}")
 
 
 @pytest.fixture()
@@ -36,8 +36,8 @@ def create_object():
     with allure.step("Создаем объект для теста"):
         body = {"name": "Новый объект", "data": {"объект": "ВОЗДУХ"}}
         headers = {"Content-Type": "application/json"}
-        response = requests.post('http://167.172.172.115:52353/object',
-                                 json=body,
-                                 headers=headers)
-        post_id_for_test = response.json()['id']
+        response = requests.post(
+            "http://167.172.172.115:52353/object", json=body, headers=headers
+        )
+        post_id_for_test = response.json()["id"]
         return post_id_for_test
